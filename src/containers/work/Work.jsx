97103fe9692a9e80cbbae7459cdb367bea955f13
liohumb <helloliohumb@gmail.com>
2023-03-04 02:13:01 +0100

    mise en place de la section work avec animation hover css

--- src/containers/work/Work.jsx
+++ src/containers/work/Work.jsx
@@ -1,9 +1,20 @@
+import {works} from '../../data/works.js'
+
 import './work.scss'
 
 export default function Work() {
     return (
         <section className="work section">
-            Work
+            <div className="work__container">
+                <div className="work__container-left">
+                    <ul className="work__content">
+                        {works.filter(work => work.container).map((work) => (
+                            <li className="work__content-name" key={work.id} data-text={work.name}>{work.name}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="work__container-right"></div>
+            </div>
         </section>
     )
 }
---
+++ src/data/works.js
@@ -0,0 +1,106 @@
+export const works = [
+    {
+        id: 1,
+        name: 'L&A WoodFactory',
+        logo: './images/works/logo/lawoodfactory.jpg',
+        mockup: '',
+        url: 'https://lawoodfactory.fr/',
+        git: null,
+        techno:
+            [
+                'PHP', 'Symfony', 'Twig', 'CSS'
+            ],
+        container: false
+    },
+    {
+        id: 2,
+        name: 'Le Soubeyran',
+        logo: './images/works/logo/lesoubeyran.png',
+        mockup: '',
+        url: 'https://le-soubeyran.fr/',
+        git: 'https://github.com/liohumb/lesoubeyran',
+        techno:
+            [
+                'HTML', 'SASS', 'JavaScript', 'Three.JS', 'GSAP'
+            ],
+        container: false
+    },
+    {
+        id: 3,
+        name: 'Les Ailes de Pégase',
+        logo: './images/works/logo/lesailesdepegase.png',
+        mockup: '',
+        url: 'https://lesailesdepegase.fr/',
+        git: 'https://github.com/liohumb/lesailesdepegase',
+        techno:
+            [
+                'HTML', 'SASS', 'GSAP'
+            ],
+        container: true
+    },
+    {
+        id: 4,
+        name: 'Disney+',
+        logo: './images/works/logo/disneyplus.png',
+        mockup: '',
+        url: null,
+        git: 'https://github.com/liohumb/disneyplusClone',
+        techno:
+            [
+                'React', 'SASS'
+            ],
+        container: false
+    },
+    {
+        id: 5,
+        name: 'Portfolio',
+        logo: null,
+        mockup: '',
+        url: 'https://humb.dev',
+        git: null,
+        techno:
+            [
+                'React', 'SASS', 'ThreeJS', 'Vite'
+            ],
+        container: true
+    },
+    {
+        id: 6,
+        name: 'Yoga EveryWhere',
+        logo: './images/works/logo/yogaeverywhere.png',
+        mockup: '',
+        url: null,
+        git: null,
+        techno:
+            [
+                'React', 'Node', 'SASS', 'ThreeJS', 'GSAP'
+            ],
+        container: true
+    },
+    {
+        id: 7,
+        name: 'Label Cars',
+        logo: './images/works/logo/labelcars.png',
+        mockup: '',
+        url: null,
+        git: 'https://github.com/liohumb/label-cars-react-native',
+        techno:
+            [
+                'React Native', 'React', 'Node', 'Style Component', 'SASS'
+            ],
+        container: true
+    },
+    {
+        id: 8,
+        name: 'Entre 2 Mains',
+        logo: '',
+        mockup: '',
+        url: null,
+        git: 'https://github.com/liohumb/entre2mains-web',
+        techno:
+            [
+                'React', 'Node', 'SASS'
+            ],
+        container: true
+    }
+]
