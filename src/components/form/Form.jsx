5b64887ef808208c2dd6a095dc56fa2145688b1a
liohumb <helloliohumb@gmail.com>
2023-03-04 12:11:15 +0100

    mise en place de l'envoie d'email avec emailjs

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,8 +1,25 @@
+import {useRef, useState} from 'react'
+import emailjs from '@emailjs/browser'
+
 import './form.scss'
 
 export default function Form() {
+    const form = useRef()
+    const [success, setSuccess] = useState(null)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm('service_o38e22i', 'template_5k53zku', form.current, 'B9Cq2ujwmd-LYBb_e')
+            .then((result) => {
+                console.log(result.text)
+                setSuccess(true)
+            }, (error) => {
+                console.log(error.text)
+                setSuccess(false)
+            })
+    }
     return (
-        <form className="form">
+        <form className="form" ref={form} onSubmit={handleSubmit}>
             <div className="form__container">
                 <label htmlFor="name"></label>
                 <input type="text" name="name" id="name" placeholder="Votre nom"/>
@@ -10,7 +27,11 @@ export default function Form() {
                 <input type="email" name="email" id="email" placeholder="votre@email.com"/>
                 <label htmlFor="content"></label>
                 <textarea name="content" id="content" placeholder="Dites moi tout..."></textarea>
-                <button type="submit">Envoyer</button>
+                {success ?
+                    <span>Votre message a bien été envoyé :)</span>
+                :
+                    <button type="submit">Envoyer</button>
+                }
             </div>
         </form>
     )
